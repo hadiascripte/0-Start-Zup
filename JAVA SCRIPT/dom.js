783e0e0c62d9bd13e9d1968214af41f9005f99fa
hadiascripte <hadiascripte@gmail.com>
2025-04-04 18:17:35 +0200

    Exercices

--- JAVA SCRIPT/dom.js
+++ JAVA SCRIPT/dom.js
@@ -1,6 +1,6 @@
 // // Sélection d'éléments dans le DOM
 
-// //Sélectionner le premier élément (par son ID ou sa class) 
+// //Sélectionner le premier élément (par son ID ou sa class)
 // // Il ne selectionnera que le premier élément trouvé dans le DOM
 
 // document.querySelector("h1");
@@ -26,7 +26,6 @@
 // // On peut utiliser la propriété style.color et lui assigner une couleur
 // li.style.color = "red";
 
-
 // //Attributs
 // //On peut ajouter un attribut à un élément en utilisant la méthode setAttribute
 
@@ -37,18 +36,12 @@
 
 //  // On peut cacher un élément en lui ajoutant l'attribut hidden
 
-
 // // Afficher l'interieur d'un élément
 
 // // On peut afficher l'interieur d'un élément en utilisant la propriété innerHTML ou innerText
 
 // li.innerHTML = "Je suis un élément fort en innerHTML"; // On peut ajouter du HTML à l'interieur d'un élément
 // li.innerText = "Je suis un élément en innerText"; // On peut ajouter du texte à l'interieur d'un élément
-
-
-
-
-
 
 // // Ajouter un élément dans le DOM
 // // On peut ajouter un élément dans le DOM en utilisant la méthode appendChild.
@@ -58,7 +51,6 @@
 // const li1 = document.createElement("li"); // On crée un élément li
 // li1.innerText = "Je suis un NOUVEL élément li"; // On lui ajoute du texte
 // document.querySelector("ul").appendChild(li1); // On ajoute l'élément li1 à la fin de la liste ul
-
 
 // //  EVENTS (Ecouteurs d'événements)
 // // On peut ajouter un écouteur d'événement à un élément en utilisant la méthode addEventListener
@@ -70,16 +62,49 @@
 //     alert("Vous avez cliqué sur le bouton !"); // On affiche une alerte lorsque le bouton est cliqué
 // })
 
-
-
+// // On peut écouter un événement sur un élément en utilisant la méthode addEventListener
 //Ajouter un evenement au clic sur un bouton
+
+
+
+let monTitre= document.querySelector("h1");
+
+
+
 
 let compteur = 0; // On initialise un compteur à 0
 let btn = document.querySelector("#btn"); // On sélectionne le bouton
 let affichage = document.querySelector("#compteur"); // On sélectionne l'élément qui va afficher le compteur
+let btn2 = document.querySelector("#btn2")
+btn2.setAttribute("hidden","hidden");
+// On ajoute un écouteur d'événement au clic sur le bouton
 
-
-btn.addEventListener("click", function() { // On ajoute un écouteur d'événement au clic sur le bouton
-    compteur++; // On incrémente le compteur de 1
-    affichage.innerHTML = "Nombre de clics : " + compteur; // On affiche le compteur dans l'élément sélectionné
+btn.addEventListener("click", function () {
+    compteur++;
+     
+    affichage.innerHTML = "Nombre de clics : " + compteur;
+    if (compteur == 5) {
+        compteur = 0;
+        affichage.innerHTML =
+            "Vous avez atteint le nombre maximum de clics ! CALM DOWN ...";
+        btn.setAttribute("hidden", "hidden");
+            btn2.removeAttribute("hidden");
+    }
 })
+btn2.addEventListener("click", function () {
+      btn.removeAttribute("hidden");
+      btn2.setAttribute("hidden","hidden")
+    
+});
+
+
+//changer la couleur de fond d'un élément au clic
+
+let monBtn = document.querySelector("#body");
+body = document.querySelector("body");
+let color = ["red", "green", "yellow", "pink", "purple"];
+
+colorBody.addEventListener("click", function () {
+    body.style.backgroundColor = color[Math.floor(Math.random() * color.length)];
+});
+
